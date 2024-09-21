8759d472bee65b1692b6674337b6e34c9a1c0dc4
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-09-21 11:14:25 +0700

    [REFACTOR] Refactored actions.ts code to be more clean

--- lib/auth/register/actions.ts
+++ lib/auth/register/actions.ts
@@ -4,21 +4,28 @@ import { RegisterForm } from "@/types/auth/register"
 import { cookies } from "next/headers"
 
 export async function handleFormSubmit(data: RegisterForm): Promise<{ ok: boolean, message: string }> {
-  const res = await fetch(`${process.env.API_BASE_URL}/api/auth/register`, {
-    method: "POST", 
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
+  const reqUrl = `${process.env.API_BASE_URL}/api/auth/register`
+  try {
+    const res = await registerRequest(reqUrl, data)
+    
+    if (res.ok) {
+      await setRefreshAndAccessTokens(res)
+      return { ok: true, message: "Registrasi berhasil" }
+    }
   
-  if (res.ok) {
-    await setRefreshAndAccessTokens(res)
-    return { ok: true, message: "Registrasi berhasil" }
+    const errorResponse = await res.json()
+    return { ok: false, message: errorResponse.detail }
+  } catch (error) {
+    return { ok: false, message: "Terjadi kesalahan pada registrasi"  }
   }
+}
 
-  const jsonRes = await res.json()
-  return { ok: false, message: jsonRes.detail }
+async function registerRequest(url: string, data: RegisterForm): Promise<Response> {
+  return fetch(url, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  });
 }
 
 async function setRefreshAndAccessTokens(response: Response) {
