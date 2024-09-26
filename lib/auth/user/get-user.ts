b3808aefc470d3765f255d9a42ece2dd913d24a4
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-09-26 21:18:24 +0700

    FIX: changed API route for getting current user

--- lib/auth/user/get-user.ts
+++ lib/auth/user/get-user.ts
@@ -3,7 +3,7 @@ import User from "@/types/auth/user";
 const API_URL = `http://127.0.0.1:8000`;
 
 export async function getUser(token?: string): Promise<User | null> {
-  const response = await fetch(API_URL, {
+  const response = await fetch(`${API_URL}/api/auth/me`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
