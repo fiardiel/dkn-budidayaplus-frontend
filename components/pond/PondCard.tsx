64e7299ebcb8cbdf3e9da1fdb177d43bf7d2d8fd
fiardiel <rafi.ardiel@ui.ac.id>
2024-09-26 16:38:57 +0700

    [REFACTOR] Added meter cube text to volume

--- components/pond/PondCard.tsx
+++ components/pond/PondCard.tsx
@@ -25,7 +25,7 @@ const PondCard: React.FC<PondCardProps> = ({ pond }) => {
           </CardTitle>
         </CardHeader>
         <CardContent className='pb-2'>
-          <p>Volume: {pond.depth * pond.width * pond.length}</p>
+          <p>Volume: {pond.depth * pond.width * pond.length} m<sup>3</sup></p>
         </CardContent>
         <CardFooter>
           <Button className='bg-blue-500 hover:bg-blue-600 active:bg-blue-700' asChild>
