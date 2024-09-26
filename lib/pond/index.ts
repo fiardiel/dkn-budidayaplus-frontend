2dd82d429ff63e6bc9447db2d13328d8ca2f408f
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-09-26 22:02:08 +0700

    chore: changed the message in schema and added imports for server actions

--- lib/pond/index.ts
+++ lib/pond/index.ts
@@ -2,5 +2,6 @@ import { fetchPonds } from "@/lib/pond/fetchPonds";
 import { fetchPond } from "@/lib/pond/fetchPond";
 import { addPond } from "@/lib/pond/addPond";
 import { updatePond } from "@/lib/pond/updatePond";
+import { deletePond } from "@/lib/pond/deletePond";
 
-export {fetchPonds, fetchPond, addPond, updatePond};
+export {fetchPonds, fetchPond, addPond, deletePond, updatePond};
