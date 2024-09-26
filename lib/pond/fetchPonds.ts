99594aa45bab0c4a528b99d15e672b994bda2aca
fiardiel <rafi.ardiel@ui.ac.id>
2024-09-26 14:13:45 +0700

    [REFACTOR] Changed the type of Pond regarding the backend change

--- lib/pond/fetchPonds.ts
+++ lib/pond/fetchPonds.ts
@@ -2,7 +2,7 @@ import { Pond } from "@/types/pond";
 
 export async function fetchPonds(accessToken?: string): Promise<Pond[]> {
   try {
-    const res = await fetch(`${process.env.API_BASE_URL}/pond`, {
+    const res = await fetch(`${process.env.API_BASE_URL}/api/pond/`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
