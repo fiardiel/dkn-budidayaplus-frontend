54c64fc630afafb6c3f4ae012baad81487841d48
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-18 20:32:41 +0700

    [RED] TEST: adjusted test for the new simplified cycle modal plan

--- __tests__/cycle/cycle-modal.test.tsx
+++ __tests__/cycle/cycle-modal.test.tsx
@@ -3,6 +3,7 @@ import { AddCycleModal } from '@/components/cycle';
 import { createCycle } from '@/lib/cycle';
 import { Pond } from '@/types/pond';
 import { fetchPonds } from '@/lib/pond';
+import { addDays, format } from 'date-fns';
 
 jest.mock('@/lib/cycle', () => ({
   createCycle: jest.fn(),
@@ -11,6 +12,11 @@ jest.mock('@/lib/cycle', () => ({
 jest.mock('@/lib/pond', () => ({
   fetchPonds: jest.fn()
 }))
+
+const currentDate = new Date()
+const currentMonth = currentDate.getMonth()
+const currentYear = currentDate.getFullYear()
+const dateForTest = new Date(currentYear, currentMonth, 15)
 
 const mockPonds: Pond[] = [
   {
@@ -47,13 +53,10 @@ describe('Add Cycle Modal', () => {
     await act(async () => {
       fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
     });
-
-    expect(screen.getByPlaceholderText('Tanggal Mulai')).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('Tanggal Selesai')).toBeInTheDocument();
-    expect(screen.getByText('Pond 1')).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 1')).toBeInTheDocument();
-    expect(screen.getByText('Pond 2')).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 2')).toBeInTheDocument();
+    
+    expect(screen.getByRole('button', { name: /tanggal mulai/i })).toBeInTheDocument();
+    expect(screen.getByLabelText('Jumlah ikan kolam Pond 1')).toBeInTheDocument();
+    expect(screen.getByLabelText('Jumlah ikan kolam Pond 2')).toBeInTheDocument();
   });
 
 
@@ -67,11 +70,23 @@ describe('Add Cycle Modal', () => {
       expect(fetchPonds).toHaveBeenCalled();
     });
 
-    fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Mulai'), { target: { value: '2021-08-01' } });
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Selesai'), { target: { value: '2021-09-30' } });
-    fireEvent.change(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 1'), { target: { value: '1000' } });
-    fireEvent.change(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 2'), { target: { value: '2000' } });
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
+    })
+
+    const button = screen.getByRole('button', { name: /tanggal mulai/i });
+    const fishInput1 = screen.getByLabelText('Jumlah ikan kolam Pond 1');
+    const fishInput2 = screen.getByLabelText('Jumlah ikan kolam Pond 2');
+
+    fireEvent.change(fishInput1, { target: { value: '1000' } });
+    fireEvent.change(fishInput2, { target: { value: '2000' } });
+    fireEvent.click(button);
+    
+    fireEvent.click(screen.getByRole('gridcell', { name: '15' }));
+
+    await waitFor(() => {
+      expect(screen.getByText(`Tanggal selesai: ${format(addDays(dateForTest, 60), 'PPP')}`)).toBeInTheDocument();
+    })
 
     await act(async () => {
       fireEvent.click(screen.getByRole('button', { name: /simpan/i }));
@@ -84,8 +99,10 @@ describe('Add Cycle Modal', () => {
     });
   })
 
-  it('shows error message when start date is after end date', async () => {
+
+  it('shows error message when form submission succeeds, but the server returns a non 200 response', async () => {
     (fetchPonds as jest.Mock).mockResolvedValue(mockPonds);
+    (createCycle as jest.Mock).mockResolvedValue({ success: false, message: 'Anda memiliki tambak yang bertabrakan' });
 
     render(<AddCycleModal />);
 
@@ -93,95 +110,39 @@ describe('Add Cycle Modal', () => {
       expect(fetchPonds).toHaveBeenCalled();
     });
 
-    fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
-
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Mulai'), { target: { value: '2021-08-15' } });
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Selesai'), { target: { value: '2021-08-01' } });
-
     await act(async () => {
-      fireEvent.click(screen.getByRole('button', { name: /simpan/i }));
-    });
-
-    await waitFor(() => {
-      expect(screen.getByText('Tanggal akhir harus setelah tanggal mulai')).toBeInTheDocument();
+      fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
     })
-  })
 
-  it('shows error message when cycle duration is not 60 days', async () => { 
-    (fetchPonds as jest.Mock).mockResolvedValue(mockPonds);
-    render(<AddCycleModal />);
+    const button = screen.getByRole('button', { name: /tanggal mulai/i });
+    const fishInput1 = screen.getByLabelText('Jumlah ikan kolam Pond 1');
+    const fishInput2 = screen.getByLabelText('Jumlah ikan kolam Pond 2');
 
-    await waitFor(() => {
-      expect(fetchPonds).toHaveBeenCalled();
-    });
-
-    fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
-
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Mulai'), { target: { value: '2021-08-01' } });
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Selesai'), { target: { value: '2021-08-02' } });
-
-    await act(async () => {
-      fireEvent.click(screen.getByRole('button', { name: /simpan/i }));
-    });
-
-    await waitFor(() => {
-      expect(screen.getByText('Durasi siklus harus 60 hari')).toBeInTheDocument();
-    })
-  })
-
-  it('shows error message when the server has an error, not reached, or invalid input from the server', async () => {
-    (createCycle as jest.Mock).mockRejectedValue(new Error('Gagal membuat siklus tambak'));
-    (fetchPonds as jest.Mock).mockResolvedValue(mockPonds);
-
-    render(<AddCycleModal />);
-
-    await waitFor(() => {
-      expect(fetchPonds).toHaveBeenCalled();
-    });
-
-    fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Mulai'), { target: { value: '2021-08-01' } });
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Selesai'), { target: { value: '2021-09-30' } });
-    fireEvent.change(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 1'), { target: { value: '1000' } });
-    fireEvent.change(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 2'), { target: { value: '2000' } });
-
-    await act(async () => {
-      fireEvent.click(screen.getByRole('button', { name: /simpan/i }));
-    });
-
-    await waitFor(() => {
-      expect(screen.getByText('Gagal membuat siklus tambak')).toBeInTheDocument();
-    });
-  })
-
-  it('doesnt allow form submission when any of the fields are invalid', async () => { 
-    (fetchPonds as jest.Mock).mockResolvedValue(mockPonds);
+    fireEvent.change(fishInput1, { target: { value: '1000' } });
+    fireEvent.change(fishInput2, { target: { value: '2000' } });
+    fireEvent.click(button);
     
-    render(<AddCycleModal />);
+    fireEvent.click(screen.getByRole('gridcell', { name: '15' }));
 
     await waitFor(() => {
-      expect(fetchPonds).not.toHaveBeenCalled();
-    });
-
-    fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
-
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Mulai'), { target: { value: '2021-08-01' } });
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Selesai'), { target: { value: '2021-09-30' } });
-    fireEvent.change(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 1'), { target: { value: '1000' } });
-    fireEvent.change(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 2'), { target: { value: '-2000' } });
+      expect(screen.getByText(`Tanggal selesai: ${format(addDays(dateForTest, 60), 'PPP')}`)).toBeInTheDocument();
+    })
 
     await act(async () => {
       fireEvent.click(screen.getByRole('button', { name: /simpan/i }));
     });
 
+    expect(createCycle).toHaveBeenCalled();
+
     await waitFor(() => {
-      expect(createCycle).not.toHaveBeenCalled();
-    })
+      expect(screen.getByText('Anda memiliki tambak yang bertabrakan')).toBeInTheDocument();
+    });
   })
 
-  it('does not close the modal when there is an error', async () => {
+  
+  it('shows error message when form submission fails', async () => {
     (fetchPonds as jest.Mock).mockResolvedValue(mockPonds);
-    (createCycle as jest.Mock).mockRejectedValue(new Error('Gagal membuat siklus tambak'));
+    (createCycle as jest.Mock).mockRejectedValue(new Error('Network Error'));
 
     render(<AddCycleModal />);
 
@@ -189,32 +150,70 @@ describe('Add Cycle Modal', () => {
       expect(fetchPonds).toHaveBeenCalled();
     });
 
-    fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Mulai'), { target: { value: '2021-08-01' } });
-    fireEvent.change(screen.getByPlaceholderText('Tanggal Selesai'), { target: { value: '2021-09-30' } });
-    fireEvent.change(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 1'), { target: { value: '1000' } });
-    fireEvent.change(screen.getByPlaceholderText('Jumlah ikan pada kolam Pond 2'), { target: { value: '2000' } });
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
+    })
+
+    const button = screen.getByRole('button', { name: /tanggal mulai/i });
+    const fishInput1 = screen.getByLabelText('Jumlah ikan kolam Pond 1');
+    const fishInput2 = screen.getByLabelText('Jumlah ikan kolam Pond 2');
+
+    fireEvent.change(fishInput1, { target: { value: '1000' } });
+    fireEvent.change(fishInput2, { target: { value: '2000' } });
+    fireEvent.click(button);
+    
+    fireEvent.click(screen.getByRole('gridcell', { name: '15' }));
+
+    await waitFor(() => {
+      expect(screen.getByText(`Tanggal selesai: ${format(addDays(dateForTest, 60), 'PPP')}`)).toBeInTheDocument();
+    })
 
     await act(async () => {
       fireEvent.click(screen.getByRole('button', { name: /simpan/i }));
     });
 
+    expect(createCycle).toHaveBeenCalled();
+
     await waitFor(() => {
-      expect(screen.getByRole('dialog')).toBeInTheDocument();
-    })
+      expect(screen.getByText('Network Error')).toBeInTheDocument();
+    });
   })
 
-  it('shows message when there are no ponds available', async () => {
-    (fetchPonds as jest.Mock).mockResolvedValue([]);
 
+  it('shows error message if fish amount is filled 0', async () => {
+    (fetchPonds as jest.Mock).mockResolvedValue(mockPonds);
     render(<AddCycleModal />);
 
     await waitFor(() => {
       expect(fetchPonds).toHaveBeenCalled();
     });
 
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: /mulai siklus/i }));
+    })
+
+    const button = screen.getByRole('button', { name: /tanggal mulai/i });
+    const fishInput1 = screen.getByLabelText('Jumlah ikan kolam Pond 1');
+    const fishInput2 = screen.getByLabelText('Jumlah ikan kolam Pond 2');
+
+    fireEvent.change(fishInput1, { target: { value: '0' } });
+    fireEvent.change(fishInput2, { target: { value: '2000' } });
+    fireEvent.click(button);
+    
+    fireEvent.click(screen.getByRole('gridcell', { name: '15' }));
+
     await waitFor(() => {
-      expect(screen.getByText('Tidak ada kolam yang tersedia')).toBeInTheDocument();
+      expect(screen.getByText(`Tanggal selesai: ${format(addDays(dateForTest, 60), 'PPP')}`)).toBeInTheDocument();
+    })
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: /simpan/i }));
+    });
+
+    expect(createCycle).not.toHaveBeenCalled();
+
+    await waitFor(() => {
+      expect(screen.getByText('Jumlah ikan harus lebih besar dari 0')).toBeInTheDocument();
     });
   })
 })
