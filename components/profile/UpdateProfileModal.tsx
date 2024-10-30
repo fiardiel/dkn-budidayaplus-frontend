47530e6e33f2b1268dafea77ebbe6322d51ffe38
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-31 00:06:48 +0700

    [GREEN] FEAT: Implemented update profile modal

--- components/profile/UpdateProfileModal.tsx
+++ components/profile/UpdateProfileModal.tsx
@@ -1,11 +1,28 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button'
+import { Modal as DialogContent } from '@/components/ui/modal'
+import { UpdateProfileForm } from '@/components/profile'
+import { useProfile } from '@/hooks/useProfile'
 
 const UpdateProfileModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const profile = useProfile()
+
   return (
     <div>
-
+      <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogTrigger asChild>
+          <Button>
+            Update Profile
+          </Button>
+        </DialogTrigger>
+        <DialogContent title='Update Profile'>
+          <UpdateProfileForm profile={profile} setIsModalOpen={setIsModalOpen} />
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
