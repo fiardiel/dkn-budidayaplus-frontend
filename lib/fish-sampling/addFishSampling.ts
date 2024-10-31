190506217d432f86b8b0c3ae10f26ad1892b79c3
Rafif Aulia <rafif.firmansyah@gmail.com>
2024-10-31 19:04:17 +0700

    [RED] REFACTOR: Implement backend calling function for REFACTORED Fish Sampling with Cycle

--- lib/fish-sampling/addFishSampling.ts
+++ lib/fish-sampling/addFishSampling.ts
@@ -4,33 +4,32 @@ import { cookies } from "next/headers";
 import { formDataToObject } from "@/lib/utils";
 
 export async function addFishSampling(
-  data: FormData,
-  pondId?: string,
-  fishSamplingId?: string
+  pondId: string, 
+  cycleId: string, 
+  data: FormData
 ): Promise<{ success: boolean; message?: string }> {
+  const API_BASE_URL = process.env.API_BASE_URL;
+  
   const token = cookies().get('accessToken')?.value;
+  const apiUrl = `${API_BASE_URL}/api/fish-sampling/${pondId}/${cycleId}/`;
+  const samplingData = formDataToObject(data);
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(samplingData),
+    });
 
-  const baseUrl = process.env.API_BASE_URL;
-  const samplingPath = fishSamplingId ? fishSamplingId + '/' : '';
-  const apiUrl = `${baseUrl}/api/fish-sampling/${pondId}/${samplingPath}`;
-
-  const fishSamplingData = formDataToObject(data)
-
-  const response = await fetch(apiUrl, {
-    method: fishSamplingId ? 'PUT' : 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-        ...fishSamplingData,
-      }),
-  });
-
-  if (response.ok) {
-    return { success: true, message: 'Fish sampling data submitted successfully' };
-  } else {
-    const errorResponse = await response.json();
-    return { success: false, message: errorResponse?.message || 'Failed to submit fish sampling data' };
+    if (response.ok) {
+      return { success: true, message: 'Data pengambilan sampel ikan berhasil ditambahkan' };
+    } else {
+      const errorResponse = await response.json();
+      return { success: false, message: errorResponse?.message || 'Gagal menambahkan data pengambilan sampel ikan' };
+    }
+  } catch (error) {
+    throw new Error("Gagal terhubung ke server");
   }
 }
