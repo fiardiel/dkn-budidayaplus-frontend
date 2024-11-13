fa7d2387bacba3a943036a6965e1b4b37d3451eb
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-13 15:21:14 +0700

    [GREEN] FEAT: implemented task page and some coverage for other task components

--- app/task/page.tsx
+++ app/task/page.tsx
@@ -1,9 +1,14 @@
+import { Tasks } from '@/components/tasks'
+import { fetchTasks } from '@/lib/tasks'
 import React from 'react'
 
 const TasksPage = async () => {
+  const tasks = await fetchTasks()
   return (
-    <div>
-
+    <div className='flex mt-20 justify-center'>
+      <div className='w-[80%] py-10'>
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   )
 }
