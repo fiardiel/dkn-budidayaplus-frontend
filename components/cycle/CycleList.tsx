e235745123d3df200df3e98baff1d6b83b8f6739
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-20 18:10:37 +0700

    [GREEN] FEAT: created implementation for cycle list component

--- components/cycle/CycleList.tsx
+++ components/cycle/CycleList.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { getCycleList } from '@/lib/cycle'
+import { CycleCarousel } from '@/components/cycle'
 
 interface CycleListProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 const CycleList: React.FC<CycleListProps> = async ({ ...props }) => {
+  const cycleList = await getCycleList()
+
   return (
     <div {...props}>
+      <CycleCarousel cycleList={cycleList} />
     </div>
   )
 }
