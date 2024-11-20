f745d3dc077d546e79648fad5206c0160440484a
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-20 12:05:18 +0700

    [GREEN] FEAT: created implementation for cycle carousel

--- components/cycle/CycleCarousel.tsx
+++ components/cycle/CycleCarousel.tsx
@@ -2,16 +2,56 @@
 
 import { CycleList } from '@/types/cycle'
 import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+} from "@/components/ui/carousel"
+import { cn } from '@/lib/utils'
+import { renderCycleCard } from '@/components/cycle/renderCycleCard'
 
 interface CycleCarouselProps extends React.HTMLAttributes<HTMLDivElement> {
   cycleList: CycleList
 }
 
 const CycleCarousel: React.FC<CycleCarouselProps> = ({ cycleList }) => {
-  return (
-    <div>
+  const totalLength = cycleList.active.length + cycleList.past.length + cycleList.future.length
 
-    </div>
+  return (
+    <Carousel
+      opts={{
+        startIndex: cycleList.past.length
+      }}
+      className="max-w-full"
+    >
+      <CarouselContent className={cn(
+        totalLength > 1 ? 'justify-start' : 'justify-center'
+      )}>
+        {cycleList.past.map((cycle) =>
+          renderCycleCard(
+            cycle,
+            'Siklus Lalu',
+            'bg-slate-500',
+            'text-slate-400'
+          )
+        )}
+        {cycleList.active.map((cycle) =>
+          renderCycleCard(
+            cycle,
+            'Siklus Aktif',
+            'bg-primary-600',
+            'text-primary-300'
+          )
+        )}
+        {cycleList.future.map((cycle) =>
+          renderCycleCard(
+            cycle,
+            'Siklus Mendatang',
+            'bg-slate-500',
+            'text-slate-400'
+          )
+        )}
+      </CarouselContent >
+    </Carousel >
   )
 }
 
