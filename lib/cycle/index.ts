773dfa1411b14036fe2001f34b82ab31d8336886
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-11-21 16:51:39 +0700

    chore: added stop cycle index

--- lib/cycle/index.ts
+++ lib/cycle/index.ts
@@ -1,3 +1,4 @@
 export { createCycle } from "@/lib/cycle/createCycle";
 export { getLatestCycle } from "@/lib/cycle/getLatestCycle";
 export { getCycleList } from "@/lib/cycle/getCycleList";
+export { stopCycle } from "@/lib/cycle/stopCycle";
