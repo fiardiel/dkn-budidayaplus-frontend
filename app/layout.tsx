705ae3f1d2d4732ce19a9435797f5469f2bb4b69
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-29 01:50:49 +0700

    [IR] Set deployment for google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Sidebar } from "@/components/sidebar";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,11 +21,12 @@ export default function RootLayout({
       <body
         className={`${inter.className}`}
       >
-        <Sidebar/>
+        <Sidebar />
         <main>
           {children}
         </main>
       </body>
+      <GoogleAnalytics gaId="G-X7DBE88P0J" />
     </html>
   );
 }
